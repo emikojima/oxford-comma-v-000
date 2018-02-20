528336d70e722a3aabb6ce89de5e1038fb2fc234
emi kojima <emi8emi@gmail.com>
2018-02-20 22:22:45 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -2,9 +2,18 @@ def oxford_comma(array)
   new_array = []
   if array.count <= 2
   array.join(" and ")
-  
   else
     new_array << array.pop
     return "#{array.join(", ")}, and #{new_array.join}"
   end
+end
+---------------
+SOLUTION FROM GITHUB
+#def oxford_comma(array)
+  if array.length == 2
+    return "#{array[0]} and #{array[1]}"
+  elsif 2 < array.length
+    array[-1].insert(0, "and ")
+  end
+  array.join(", ")    
 end
